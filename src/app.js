95d0181300491a73cf94ce87314e4d80afac404f
Asim Ali <asimuaf41@gmail.com>
2026-07-01 00:00:01 +0500

    update code

--- src/app.js
+++ src/app.js
@@ -5,13 +5,12 @@ import { chatRouter } from './routes/chat.routes.js';
 const app = express();
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
   res.json({ ok: true });
 });
-console.log("Backend version: 2");
+
 app.use('/api', chatRouter);
 
 export { app };
