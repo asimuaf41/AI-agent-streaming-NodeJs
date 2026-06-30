ec8a346b1b6fc0591feb6159714b96e0c63b7329
Asim Ali <asimuaf41@gmail.com>
2026-07-01 00:20:50 +0500

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'express-cors';
 import { chatRouter } from './routes/chat.routes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
+
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
